8417cee5f071519c07e2465a73b283965c197173
Aliaksandr Malko <104769289+Kerigan17@users.noreply.github.com>
2023-07-12 16:37:46 +0300

    add popup

--- frontend/src/components/income.js
+++ frontend/src/components/income.js
@@ -1,3 +1,10 @@
 export class Income {
+    constructor() {
+        let deleteButtons = Array.from(document.getElementsByClassName('delete'));
+        const popup = document.getElementById('popup');
 
+        deleteButtons.forEach(item => item.onclick = () => {
+            popup.style.display = 'flex';
+        })
+    }
 }
--- frontend/src/components/incomeAndExpenses.js
+++ frontend/src/components/incomeAndExpenses.js
@@ -1,3 +1,10 @@
 export class IncomeAndExpenses {
+    constructor() {
+        const popup = document.getElementById('popup');
+        const buckets = Array.from(document.getElementsByClassName('bucket'));
 
+        buckets.forEach(item => item.onclick = () => {
+            popup.style.display = 'flex';
+        })
+    }
 }
