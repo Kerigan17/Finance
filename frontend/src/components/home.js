91d5365f5d1e827ab4cd91f0dd4be858c2bc93ea
Kerigan17 <104769289+Kerigan17@users.noreply.github.com>
2023-07-14 01:30:07 +0300

    start create table

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,7 +1,9 @@
 import {CustomHttp} from "../services/custom-http.js";
 import config from "../../config/config.js";
-import { Chart, registerables } from 'chart.js';
+import {Chart, registerables} from 'chart.js';
+
 Chart.register(...registerables);
+
 export class Home {
     constructor() {
         const ctx = document.getElementById('pieChart');
@@ -11,74 +13,75 @@ export class Home {
             new bootstrap.Tooltip(tooltipTriggerEl)
         })
 
-        this.paintDiagram();
+        //this.balance = this.getBalance();
+        //document.getElementById('balance').innerText = this.balance;
+
         this.getOperations();
     }
 
     async getOperations() {
+        this.response = null;
+
         try {
-            const response = await CustomHttp.request(config.host + '/operations' + '?period=all', 'GET', );
-            console.log(response)
+            this.response = await CustomHttp.request(config.host + '/operations' + '?period=all', 'GET',);
+            console.log(this.response)
         } catch (error) {
             console.log(error);
         }
+
+
     }
 
     async getBalance() {
         try {
-            const response = await CustomHttp.request(config.host + '/balance', 'GET', );
+            const response = await CustomHttp.request(config.host + '/balance', 'GET',);
             document.getElementById('balance').innerText = response.balance;
             console.log(response.balance)
-            return await response.balance;
         } catch (error) {
             console.log(error);
         }
     }
 
-    paintDiagram() {
-        const ctx = Array.from(document.getElementsByClassName('myChart'));
-
-        ctx.forEach(item => {
-            new Chart(item, {
-                type: 'pie',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true,
-                            display: false
-                        },
-                        x: {
-                            display: false
-                        }
-                    },
-                    gridLines: {
+    paintDiagram(diagram, items) {
+        new Chart(diagram, {
+            type: 'pie',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
                         display: false
                     },
+                    x: {
+                        display: false
+                    }
+                },
+                gridLines: {
+                    display: false
+                },
+                legend: {
+                    display: true,
+                    position: 'bottom',
+                    labels: {
+                        fontColor: 'rgb(255, 99, 132)'
+                    }
+                },
+                plugins: {
                     legend: {
-                        display: true,
-                        position: 'bottom',
+                        position: 'top',
                         labels: {
-                            fontColor: 'rgb(255, 99, 132)'
-                        }
-                    },
-                    plugins: {
-                        legend: {
-                            position: 'top',
-                            labels: {
-                                boxWidth: 35,
-                            }
+                            boxWidth: 35,
                         }
                     }
                 }
-            });
-        })
+            }
+        });
     }
 }
--- frontend/src/components/incomeAndExpenses.js
+++ frontend/src/components/incomeAndExpenses.js
@@ -5,22 +5,88 @@ export class IncomeAndExpenses {
     constructor() {
         const popup = document.getElementById('popup');
         const buckets = Array.from(document.getElementsByClassName('bucket'));
+        this.tableBody = document.getElementById('table-body');
 
         buckets.forEach(item => item.onclick = () => {
             popup.style.display = 'flex';
         })
 
-        console.log(localStorage.accessToken)
+        console.log(localStorage.accessToken);
+        this.getInfo();
     }
 
     async getInfo() {
+        this.response = null;
+        this.items = ['','','','',''];
+
         try {
-            const response = await CustomHttp.request(config.host + '/balance', 'GET', );
-            document.getElementById('balance').innerText = response.balance;
-            console.log(response.balance)
-            return await response.balance;
+            this.response = await CustomHttp.request(config.host + '/operations' + '?period=all', 'GET',);
+            console.log(this.response)
         } catch (error) {
             console.log(error);
+        }
+
+        for (let j = 0; j < this.response.length; j++) {
+            //создание строки
+            let trElement = document.createElement('tr');
+
+            //нумерация
+            let firstTdElement = document.createElement('td');
+            firstTdElement.innerText = (j + 1);
+            trElement.appendChild(firstTdElement);
+
+            //доход - расход
+            firstTdElement = document.createElement('td');
+            if (this.response[j].type) {
+                firstTdElement.innerText = 'доход';
+                firstTdElement.style.color = 'green';
+            } else {
+                firstTdElement.innerText = 'расход';
+                firstTdElement.style.color = 'red';
+            }
+            trElement.appendChild(firstTdElement);
+
+            //категория
+            firstTdElement = document.createElement('td');
+            firstTdElement.innerText = 'категория';
+            trElement.appendChild(firstTdElement);
+
+            //сумма
+            firstTdElement = document.createElement('td');
+            firstTdElement.innerText = this.response[j].amount + '$';
+            trElement.appendChild(firstTdElement);
+
+            //дата
+            firstTdElement = document.createElement('td');
+            let operationDate = this.response[j].date.split('-').reverse().join('.')
+            firstTdElement.innerText = operationDate;
+            trElement.appendChild(firstTdElement);
+
+            //коммент
+            firstTdElement = document.createElement('td');
+            firstTdElement.innerText = this.response[j].comment;
+            trElement.appendChild(firstTdElement);
+
+            //удаление и редактирование
+            firstTdElement = document.createElement('td');
+            firstTdElement.classList.add('svg-images');
+            let imgElement = document.createElement('img');
+            imgElement.src = '/images/bucket.svg';
+            firstTdElement.appendChild(imgElement);
+            imgElement = document.createElement('img');
+            imgElement.src = '../../images/pen.svg';
+            firstTdElement.appendChild(imgElement);
+
+            trElement.appendChild(firstTdElement);
+
+            // for (let i = 0; i < 5; i++) {
+            //     console.log(this.response[i])
+            //     let tdElement = document.createElement('td');
+            //     tdElement.innerText = 1;
+            //     trElement.appendChild(tdElement);
+            // }
+
+            this.tableBody.appendChild(trElement);
         }
     }
 }
